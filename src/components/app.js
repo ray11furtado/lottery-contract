57a8b847884071305ba7b4904859545b6ba76019
Ray Furtado <rayfurtado11@gmail.com>
2018-05-06 23:05:16 -0400

    web3 added

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,12 @@
 import React, { Component } from 'react';
+import web3 from '../web3';
 
-export default () => <div>Hello Ethereum</div>;
+export default class App extends Component {
+  render() {
+    web3.eth.getAccounts().then((res) => {
+      console.log(res);
+    });
+    return <div>Hello Ethereum</div>;
+  }
+
+}
